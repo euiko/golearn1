e48e57179577ef090cbc47f563697366d8a1244e
Candra Kharista P <kharis@beon.co.id>
2017-08-05 23:21:34 +0700

    Test using interface

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
+import "fmt"
+
+type person struct {
+	name string
+	age int
+}
+
+type Stringer interface {
+	String() string
+}
+
+func header() string{
+	return fmt.Sprintf("%-10s %-10s", "Name", "Age")
+}
+
+func (p person) String() string{
+	return fmt.Sprintf("%-10s %-10d", p.name, p.age)
+}
+
+func (p person) Integer() int{
+	return p.age
+}
+
+var human person = person {
+	"euiko",
+	10,
+}
 
 func main() {
+	fmt.Println(header())
+	fmt.Print(human)
 }
